3c507f65d5a90e474b3612df544fc3bae01bc9b8
Yovieaji13 <71000479+Yovieaji13@users.noreply.github.com>
2021-12-08 16:25:52 +0700

    Test

---
+++ home.php
@@ -0,0 +1,33 @@
+
+<html>
+   
+   <head>
+		<title>Home | Quiz2</title>
+		<link href="template/css/bootstrap.min.css" rel="stylesheet" />
+		<link href="template/css/sb-admin-2.css" rel="stylesheet" />
+		<script src="template/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="template/alert.css">
+   </head>
+   <body>
+   <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="login-panel panel panel-default">
+					<div class="panel-heading">
+                        <h3 class="panel-title">Sudah Login</h3>
+                        <div class="panel-body">
+						<form action= "login.php" method = "post" >
+						<fieldset>
+						<div class="form-group">
+							<button type="submit" class="btn btn-md btn-primary btn-block">Logout</button>
+						</div>
+						</fieldset>
+						</form>
+					</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	 </div>
+   </body>
+</html>
